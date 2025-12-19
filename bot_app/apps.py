0f57bd1fef141d6dac35db645205ef720fb83fb4
bovaqulov <bovaqulov@gmail.com>
2025-12-20 01:55:46 +0500

    deploy

--- bot_app/apps.py
+++ bot_app/apps.py
@@ -6,6 +6,7 @@ from django.apps import AppConfig
 class BotAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bot_app'
+    verbose_name = 'Asosiy'
 
     def ready(self):
         import bot_app.signals
