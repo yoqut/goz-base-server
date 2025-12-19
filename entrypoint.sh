7bf567cd7f7a4a551103113f312373728a758b1f
bovaqulov <bovaqulov@gmail.com>
2025-12-19 19:51:59 +0500

    CI/CD deploy action green/blue update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-echo "🚀 Django start..."
-
+# Django migrations
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+
+# Create superuser if needed
+python manage.py createsuper
+
+# Collect static
 python manage.py collectstatic --noinput
 
-exec gunicorn config.wsgi:application \
-    --bind 0.0.0.0:8000 \
-    --workers 4 \
-    --timeout 120 \
-    --log-level info
+# Start Gunicorn & Celery
+gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 4 &
+
